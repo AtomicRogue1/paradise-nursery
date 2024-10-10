9e11c4d7e04e032d0a833393c788ba04885fa759
Yash Verma <30554253+AtomicRogue1@users.noreply.github.com>
2024-10-10 16:44:12 +0530

    Commit #03

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,7 @@ import './LandingPage.css';
 
 function LandingPage() {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', justifyItems:'center', paddingTop:'5%' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyItems: 'center', paddingTop: '5%' }}>
             <h1>Welcome to Paradise Nursery</h1>
             <p style={{ textAlign: 'center', margin: '0 auto', width: '80%', flex: 1 }}>
                 At Paradise Nursery, we specialize in bringing the beauty of nature into your home. Our carefully curated selection
@@ -13,12 +13,10 @@ function LandingPage() {
                 space or add some greenery to your office, Paradise Nursery has the perfect plant for you. Let us help you create your
                 own slice of paradise, one plant at a time.
             </p>
-            <span style={{ textAlign: 'center', margin: '0 auto', width: '80%', flex: 1, paddingTop:'40px' }}>
-                <button>
-                    <Link style={{textDecoration:'',color:'inherit'}} to="/products">
-                        Shop Now
-                    </Link>
-                </button>
+            <span style={{ textAlign: 'center', margin: '0 auto', width: '80%', flex: 1, paddingTop: '40px' }}>
+                <Link style={{ textDecoration: '', color: 'inherit' }} to="/products">
+                    <button>Shop Now</button>
+                </Link>
             </span>
         </div >
     );
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../redux/reducers/cartActions';
+import './ProductPage.css'
 
 const products = [
-  { id: 1, name: 'Aloe Vera', description: 'Medicinal plant', price: 10, image: '/images/aloe.jpg' },
-  { id: 2, name: 'Lavender', description: 'Aromatic plant', price: 12, image: '/images/lavender.jpg' },
-  // Add more plants as needed
+  { id: 1, name: 'Aloe Vera', price: 10, image: 'https://greenripegarden.com/wp-content/uploads/2020/04/Aloe-Vera-Plant-2048x1365.jpg' },
+  { id: 2, name: 'Lavender', price: 12, image: 'https://worldoffloweringplants.com/wp-content/uploads/2017/11/Lavandula-angustifolia-English-Lavender1.jpg' },
+  { id: 3, name: 'Pansy', price: 15, image: 'https://www.agrifarming.in/wp-content/uploads/2021/07/pansy-327188_1920.jpg' },
+  { id: 4, name: 'Tulips', price: 12, image: 'https://www.thespruce.com/thmb/jANLi9M1AIYqJJEXt-X-1vP_1_0=/2206x0/filters:no_upscale():max_bytes(150000):strip_icc()/tulips-planting-and-growing-tulips-1402137-02-706093a4073642429ada61c2839e7240.jpg' },
+  { id: 5, name: 'Cactus', price: 20, image: 'https://www.thespruce.com/thmb/U2nAr_7bgYVZsch0ikAiXxS17y4=/4928x3264/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-554144227-577489063df78cb62c7629c8.jpg' },
+  { id: 6, name: 'Lily', price: 12, image: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Lilium_longiflorum_(Easter_Lily).JPG' },
+
 ];
 
 function ProductsPage() {
@@ -19,7 +24,6 @@ function ProductsPage() {
           <div className="plant-card" key={plant.id}>
             <img src={plant.image} alt={plant.name} />
             <h2>{plant.name}</h2>
-            <p>{plant.description}</p>
             <p>${plant.price}</p>
             <button onClick={() => dispatch(addToCart(plant))}>Add to Cart</button>
           </div>
