1dc964902fb51240983adc762aaaa885969e59c9
Yash Verma <30554253+AtomicRogue1@users.noreply.github.com>
2024-10-20 19:26:09 +0530

    Commit #06

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import store from '../redux/store';
+
+let currentState = store.getState()
 
 function Navbar() {
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart">Cart ({currentState.products.length})</Link>
     </nav>
   );
 }
